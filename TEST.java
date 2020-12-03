62ae980ce346597e98f563d1e95cb6862a2572f9
rumeysaozturk <74792539+rumeysaozturk@users.noreply.github.com>
2020-12-03 21:47:08 +0300

    Update TEST.java
    Test class is added

--- TEST.java
+++ TEST.java
@@ -28,5 +28,35 @@ public class TEST {
 	         //Retrieving the array
 	         JSONArray jsonArray = (JSONArray)(jsonObject.get("class labels"));
 	         System.out.println("");
+		       
+	         //Iterating the contents of the array
+	      
+	        
+	      JSONArray jsonArray1 = (JSONArray)(jsonObject.get("instances"));
+	         System.out.println("");
+	        
+	         //Iterating the contents of the array
+	    
+	        
+	        
+	      ArrayList<LABEL>listl=new ArrayList<>();
+	      
+	      for (int i=0;i<jsonArray.size();i++) {
+	    	  JSONObject a=(JSONObject) jsonArray.get(i);
+		        long id =(long) a.get("label id");
+		        String text=(String) a.get("label text");
+		        LABEL a1=new LABEL(id,text);
+		        listl.add(a1);
+		        
+	      }
+	      
+      ArrayList<INSTANCE>listl2=new ArrayList<>();
+	      
+	      for (int i=0;i<jsonArray1.size();i++) {
+	    	  JSONObject a2=(JSONObject) jsonArray1.get(i);
+		        long id1 =(long) a2.get("id");
+		        String text2=(String) a2.get("instance");
+		       INSTANCE a3=new INSTANCE(id1,text2);
+		        listl2.add(a3);
 	        
 	        
