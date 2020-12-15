ddb78a8327398e562adc6bdb022d8cff663b5ef5
berramer <74836665+berramer@users.noreply.github.com>
2020-12-15 20:44:25 +0300

    Add files via upload
    create dataset class

---
+++ DATASET.java
@@ -0,0 +1,58 @@
+import java.lang.Integer;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+public class DATASET {
+  private Long id;
+  private Long max_label;
+  private String name;
+  private  JSONArray labels;
+  private JSONArray ýnstances;
+  
+	public DATASET() {
+		// TODO Auto-generated constructor stub
+	}
+public DATASET(long id,long max_label,String name ,JSONArray label,JSONArray Instance) {
+	setId(id);
+	setMax_label(max_label);
+	setName(name);
+	setLabels(label);
+	setInstances(Instance);
+}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public JSONArray getLabels() {
+		return labels;
+	}
+	public void setLabels(JSONArray labels) {
+		this.labels = labels;
+	}
+	public JSONArray getInstances() {
+		return ýnstances;
+	}
+	public void setInstances(JSONArray instances) {
+		ýnstances = instances;
+	}
+	public long getMax_label() {
+		return max_label;
+	}
+	public void setMax_label(long max_label) {
+		this.max_label = max_label;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	
+	
+}
