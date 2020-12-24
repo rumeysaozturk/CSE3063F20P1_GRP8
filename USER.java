91ea9de1c62a0d464e97a14e90a8e5ccf10253e3
berramer <74836665+berramer@users.noreply.github.com>
2020-12-24 21:11:32 +0300

    Update USER.java

--- USER.java
+++ USER.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -5,23 +6,25 @@ import java.util.logging.SimpleFormatter;
 import java.util.*;
 
 public class USER {
-      private long userId;
+      private Long userId;
       private String name;
       private String usertype;
-      
+      private Double ConsistencyCheckProbability;
+      private ArrayList<INSTANCE>labeled=new ArrayList<INSTANCE>();
 	public USER() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public USER(long userId,String name,String usertype ) {
+	public USER(long userId,String name,String usertype,Double ConsistencyCheckProbability ) {
 		this.setUserId(userId);
 		this.setName(name);
 		this.setUsertype(usertype);
+		setConsistencyCheckProbability(ConsistencyCheckProbability);
 	}
 	
 	//Print user
 	public void printUser(Logger logger) {
-		logger.info("user id: "+this.getUserId()+ " username: "+this.getName()+" user type: "+ this.getUsertype()+ " is created.");
+		logger.info("user id: "+this.getUserId()+ " username: "+this.getName()+" user type: "+ this.getUsertype()+"user ConsistencyCheckProbability :" +this.getConsistencyCheckProbability()+ " is created.");
 	}
 
 
@@ -47,6 +50,26 @@ public class USER {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public ArrayList<INSTANCE> getLabeled() {
+		return labeled;
+	}
+
+	public void setLabeled(ArrayList<INSTANCE> labeled) {
+		this.labeled = labeled;
+	}
+	public void addInstance(INSTANCE x) {
+		this.labeled.add(x);
+	}
+	
+
+	public Double getConsistencyCheckProbability() {
+		return ConsistencyCheckProbability;
+	}
+
+	public void setConsistencyCheckProbability(Double consistencyCheckProbability) {
+		ConsistencyCheckProbability = consistencyCheckProbability;
 	}
 
 }
