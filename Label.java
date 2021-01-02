539facf4435d6692ecc987f850592057572770c8
EzgiDogruer <61564397+EzgiDogruer@users.noreply.github.com>
2021-01-02 18:53:43 +0300

    Update and rename LABEL.java to Label.java

--- Label.java
+++ Label.java
@@ -3,27 +3,27 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class LABEL implements finder {
+public class Label implements finder {
       private long labelid;
       private String text;
       ArrayList<Integer> uniqueInstance = new ArrayList<>();
-	public LABEL() {
+	public Label() {
 		// TODO Auto-generated constructor stub
 	}
-   public LABEL(long labelid , String text) {
+   public Label(long labelid , String text) {
 	   this.setLabelid(labelid);
 	   this.setText(text);
    }
    
 //check the unique instance
-public void checkUniqueInstance(INSTANCE instance,Logger logger) {
-	if(!this.uniqueInstance.contains(Integer.valueOf(String.valueOf(instance.getInstanceid())))) {
-		this.uniqueInstance.add(Integer.valueOf(String.valueOf(instance.getInstanceid())));
+public void checkUniqueInstance(Instancee instancee,Logger logger) {
+	if(!this.uniqueInstance.contains(Integer.valueOf(String.valueOf(instancee.getInstanceid())))) {
+		this.uniqueInstance.add(Integer.valueOf(String.valueOf(instancee.getInstanceid())));
 	}
-	logger.info("label id: "+this.labelid + " unique instance is calculated");
+	//logger.info("label id: "+this.labelid + " unique instance is calculated");
 }
 
-public int find(ArrayList<LABEL>listoflabel,ArrayList<INSTANCE>list,Long labelid) {
+public int find(ArrayList<User> list1,ArrayList<Label>listoflabel,ArrayList<Instancee>list,Long labelid) {
 	for(int c=0;c<listoflabel.size();c++) {
 		if(labelid==listoflabel.get(c).labelid) {
 			return c;
@@ -52,6 +52,5 @@ public void setUniqueInstance(ArrayList<Integer> uniqueInstance) {
 	this.uniqueInstance = uniqueInstance;
 }
 
-	
-	
+
 }
